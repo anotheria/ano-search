f27d0d0108dc2ebaeae74c6dd616bfc9ea776895
Hudson <jenkins@anotheria.net>
2009-04-29 09:52:45 +0000

    lib is comming from ivy now

--- java/net/anotheria/search/filteredsearch/plainengine/PlainFilteringEngineFactory.java
+++ java/net/anotheria/search/filteredsearch/plainengine/PlainFilteringEngineFactory.java
@@ -3,7 +3,7 @@ package net.anotheria.search.filteredsearch.plainengine;
 import net.anotheria.search.filteredsearch.Filterable;
 import net.anotheria.search.filteredsearch.FilteringEngine;
 
-public class PlainFilteringEngineFactory {
+public class PlainFilteringEngineFactory{
 	public static <T extends Filterable> FilteringEngine<T> createFilteringEngine(){
 		return new PlainFilteringEngine<T>();
 	}
